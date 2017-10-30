ba24d58a2e153bbbe30760e8982c9c91a61ce216
Kenneth Chen <unusualkid@gmail.com>
2017-10-30 18:48:38 +0800

    Add LongPressGesture and drop pin.

--- VirtualTourist/Controllers/MapViewController.swift
+++ VirtualTourist/Controllers/MapViewController.swift
@@ -15,8 +15,9 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(longPress(gestureRecognizer:)))
+        mapView.addGestureRecognizer(longPressGesture)
+        mapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +30,16 @@ class MapViewController: UIViewController {
         
     }
     
+    @objc func longPress(gestureRecognizer: UITapGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            let touchPoint = gestureRecognizer.location(in: mapView)
+            let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinates
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -38,5 +49,42 @@ class MapViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
+
+extension MapViewController: MKMapViewDelegate {
+    
+    // Here we create a view with a "right callout accessory view". You might choose to look into other
+    // decoration alternatives. Notice the similarity between this method and the cellForRowAtIndexPath
+    // method in TableViewDataSource.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+        let reuseId = "pin"
+
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            pinView!.animatesDrop = true
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+
+        return pinView
+    }
+//
+//    // This delegate method is implemented to respond to taps. It opens the system browser
+//    // to the URL specified in the annotationViews subtitle property.
+//    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+//        if control == view.rightCalloutAccessoryView {
+//            let app = UIApplication.shared
+//            if let toOpen = view.annotation?.subtitle! {
+//                app.open(URL(string: toOpen)!, options: [:], completionHandler: nil)
+//            }
+//        }
+//    }
+}
+
