d7e5bbca30c490bfe430cd0d66a6bde8b54f73ba
Kenneth Chen <unusualkid@gmail.com>
2017-11-29 15:51:06 +0800

    Set up basic functions for coredata in PhotoAlbumVC

--- VirtualTourist/Controllers/PhotoAlbumViewController.swift
+++ VirtualTourist/Controllers/PhotoAlbumViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class PhotoAlbumViewController: UIViewController {
     
@@ -15,6 +16,38 @@ class PhotoAlbumViewController: UIViewController {
     @IBOutlet weak var toolButton: UIBarButtonItem!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
+    // blockOperations for Coredata
+    var blockOperations: [BlockOperation] = []
+    var fetchedResultsController : NSFetchedResultsController<NSFetchRequestResult>? {
+        didSet {
+            // Whenever the frc changes, we execute the search and
+            // reload the collection
+            fetchedResultsController?.delegate = self
+            executeSearch()
+            collectionView?.reloadData()
+        }
+    }
+    // MARK: Init
+    
+    //    init(collectionView: UICollectionView) {
+    //        self.collectionView = collectionView
+    //    }
+    
+//    // Do not worry about this initializer. It has to be implemented
+//    // because of the way Swift interfaces with an Objective C
+//    // protocol called NSArchiving. It's not relevant.
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+    // MARK: Deinit
+    
+    deinit {
+        blockOperations.forEach { $0.cancel() }
+        blockOperations.removeAll(keepingCapacity: false)
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
@@ -63,6 +96,16 @@ extension PhotoAlbumViewController {
         flowLayout.minimumLineSpacing = space
         flowLayout.itemSize = CGSize(width: dimension, height: dimension)
     }
+    
+    func executeSearch() {
+        if let fc = fetchedResultsController {
+            do {
+                try fc.performFetch()
+            } catch let e as NSError {
+                print("Error while trying to perform a search: \n\(e)\n\(fetchedResultsController)")
+            }
+        }
+    }
 }
 
 extension PhotoAlbumViewController: MKMapViewDelegate {
@@ -93,12 +136,24 @@ extension PhotoAlbumViewController: MKMapViewDelegate {
 }
 
 extension PhotoAlbumViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.bounds.size.width / 4, height: 350)
+    //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    //        return CGSize(width: collectionView.bounds.size.width / 4, height: 350)
+    //    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        if let fc = fetchedResultsController {
+            return (fc.sections?.count)!
+        } else {
+            return 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        if let fc = fetchedResultsController {
+            return fc.sections![section].numberOfObjects
+        } else {
+            return 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -106,6 +161,73 @@ extension PhotoAlbumViewController: UICollectionViewDataSource, UICollectionView
         
         return cell
     }
+}
+
+extension PhotoAlbumViewController: NSFetchedResultsControllerDelegate {
+    
+    // MARK: NSFetchedResultsControllerDelegate
+    
+    func controllerWillChangeContent(controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        blockOperations.removeAll(keepingCapacity: false)
+    }
+    
+    func controller(controller: NSFetchedResultsController<NSFetchRequestResult>, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+        
+        switch type {
+            
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            let op = BlockOperation { [weak self] in self?.collectionView.insertItems(at: [newIndexPath as IndexPath]) }
+            blockOperations.append(op)
+            
+        case .update:
+            guard let newIndexPath = newIndexPath else { return }
+            let op = BlockOperation { [weak self] in self?.collectionView.reloadItems(at: [newIndexPath as IndexPath]) }
+            blockOperations.append(op)
+            
+        case .move:
+            guard let indexPath = indexPath else { return }
+            guard let newIndexPath = newIndexPath else { return }
+            let op = BlockOperation { [weak self] in self?.collectionView.moveItem(at: indexPath as IndexPath, to: newIndexPath as IndexPath) }
+            blockOperations.append(op)
+            
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            let op = BlockOperation { [weak self] in self?.collectionView.deleteItems(at: [indexPath as IndexPath]) }
+            blockOperations.append(op)
+            
+        }
+    }
+    
+    func controller(controller: NSFetchedResultsController<NSFetchRequestResult>, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+        
+        switch type {
+            
+        case .insert:
+            let op = BlockOperation { [weak self] in self?.collectionView.insertSections(NSIndexSet(index: sectionIndex) as IndexSet) }
+            blockOperations.append(op)
+            
+        case .update:
+            let op = BlockOperation { [weak self] in self?.collectionView.reloadSections(NSIndexSet(index: sectionIndex) as IndexSet) }
+            blockOperations.append(op)
+            
+        case .delete:
+            let op = BlockOperation { [weak self] in self?.collectionView.deleteSections(NSIndexSet(index: sectionIndex) as IndexSet) }
+            blockOperations.append(op)
+            
+        default: break
+            
+        }
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        collectionView.performBatchUpdates({
+            self.blockOperations.forEach { $0.start() }
+        }, completion: { finished in
+            self.blockOperations.removeAll(keepingCapacity: false)
+        })
+    }
+    
 }
 
 
